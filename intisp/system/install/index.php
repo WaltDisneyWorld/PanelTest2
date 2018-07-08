a17eb04d9b8e98f5a964712a3875914f463b53c3
Thomas Wilbur <tecflare@outlook.com>
2018-07-07 20:49:42 -0700

    pt1

---
+++ intisp/system/install/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Activate IntISP</title>
+    <link rel="stylesheet" href="style.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+  </head>
+  <body>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">
+        Activate IntISP <?php echo file_get_contents("../data/version"); ?>
+      </h1>
+      <p class="subtitle">
+        IntISP is free software, to activate your copy you will need to authorize with the adaclareID Servers.
+      </p>
+
+  <article class="message is-info">
+  <div class="message-body">
+Please make sure that you accept our terms and conditions found on our <a href="https://www.adaclare.com">site</a>.
+  </div>
+</article>
+  </div>
+      <a href="authorize.php" class="button is-link">Login and Activate</a>
+
+  </section>
+  
+   
+  </body>
+</html>
