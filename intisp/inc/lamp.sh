798e4b28129a449355fdb95c2acf4c503d9f7a0d
Thomas Wilbur <Thomas.Wilbur@my.gavilan.edu>
2018-07-25 20:51:40 -0500

    Update lamp.sh

--- intisp/inc/lamp.sh
+++ intisp/inc/lamp.sh
@@ -14,17 +14,17 @@ fi
 }
 method() {
 check php
-sudo apt-get install apache2 libapache2-mod-php mysql-server php php-cli php-fpm php-json php-mysql php-curl php-mail php-zip unzip zip -y -qq --force-yes
+sudo apt-get install apache2 libapache2-mod-php mariadb-server php php-cli php-fpm php-json php-mysql php-curl php-mail php-zip unzip zip -y -qq --force-yes
 check php
-sudo apt-get install apache2 libapache2-mod-php* mysql-server php php*-cli php*-fpm php*-json php*-mysql php*-curl php*-mail php*-zip unzip zip -y -qq --force-yes
+sudo apt-get install apache2 libapache2-mod-php* mariadb-server php php*-cli php*-fpm php*-json php*-mysql php*-curl php*-mail php*-zip unzip zip -y -qq --force-yes
 check php
 }
 methoddos() {
-sudo apt-get install apache2 libapache2-mod-php7.2 mysql-server php7.2 php7-cli php7.2-fpm php7.2-json php7.2-mysql php7.2-curl php7.2-mail php7.2-zip unzip zip -y --force-yes
+sudo apt-get install apache2 libapache2-mod-php7.2 mariadb-server php7.2 php7-cli php7.2-fpm php7.2-json php7.2-mysql php7.2-curl php7.2-mail php7.2-zip unzip zip -y --force-yes
 check php
-sudo apt-get install apache2 libapache2-mod-php7 mysql-server php7 php7-cli php7-fpm php7-json php7-mysql php7-curl php7-mail php7-zip unzip zip -y  --force-yes
+sudo apt-get install apache2 libapache2-mod-php7 mariadb-server php7 php7-cli php7-fpm php7-json php7-mysql php7-curl php7-mail php7-zip unzip zip -y  --force-yes
 check php
-sudo apt-get install apache2 libapache2-mod-php5 mysql-server php5 php5-cli php5-fpm php5-json php5-mysql php5-curl php5-mail php5-zip unzip zip -y --force-yes
+sudo apt-get install apache2 libapache2-mod-php5 mariadb-server php5 php5-cli php5-fpm php5-json php5-mysql php5-curl php5-mail php5-zip unzip zip -y --force-yes
 check php
 }
 method
