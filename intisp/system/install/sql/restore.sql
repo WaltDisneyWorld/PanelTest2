342520708729c0b15a3de4ad62f750e21e77fb25
Thomas Wilbur <Thomas.Wilbur@my.gavilan.edu>
2018-08-01 20:44:07 -0500

    Update restore.sql

--- intisp/system/install/sql/restore.sql
+++ intisp/system/install/sql/restore.sql
@@ -1,12 +1,12 @@
 -- IntISP MySQL dump
 
-SET NAMES utf8;
-SET time_zone = '+00:00';
-SET foreign_key_checks = 0;
-SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+-- SET NAMES utf8;
+-- SET time_zone = '+00:00';
+-- SET foreign_key_checks = 0;
+-- SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-CREATE DATABASE `webister` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `webister`;
+-- CREATE DATABASE `webister` /*!40100 DEFAULT CHARACTER SET latin1 */;
+-- USE `webister`;
 
 CREATE TABLE `cloudflare` (
   `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
