2b43bc73a5e64eff88782cb0ee1c8d8d55c8bdf3
Thomas Wilbur <Thomas.Wilbur@my.gavilan.edu>
2018-08-01 21:06:06 -0500

    Update writeConfig.php

--- intisp/system/install/writeConfig.php
+++ intisp/system/install/writeConfig.php
@@ -30,9 +30,15 @@ foreach(glob($path_migrations.DIRECTORY_SEPARATOR."*.sql") as $script) {
 }
 }
 echo "Restore Complete V" . file_get_contents("/var/www/html/interface/data/version") . "<br>";
+$sql = "INSERT INTO mail (id, subject, message) VALUES ('1','Welcome To Webister','<b>We are glad that you decided to choose Webister.</b> <p>We hope you enjoy our awesome control panel. You will get messages/emails once you place your email address in the settings.</p><p>
+If you feel that there are some issues or you need fix your Webister, please remember to try updating it first. You can update this in our main control panel.</p>')";
+$m->query($sql);
+$sql = "INSERT INTO settings (id, code, value) VALUES ('1', 'title', 'My Web Host')";
+$m->query($sql);
 $sql = "INSERT INTO users (id, username, password, bandwidth, diskspace, port) VALUES ('1', 'admin', '".sha1('admin'.$salt)."', '', '', '80')";
 $m->query($sql);
-echo "Admin User Created<br>";
+echo "Tables Populated Complete<br>";
+echo "Admin User Creation Complete<br>";
 $_SESSION["stp9"] = true;
 $route="after.php";
 echo '<script>window.location = "' . $route . '";</script>';
