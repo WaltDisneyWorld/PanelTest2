5f40ddbe0671a906141ca4cb1cab2ce5fad957dc
Thomas Wilbur <Thomas.Wilbur@my.gavilan.edu>
2018-08-01 21:04:34 -0500

    Delete 999_restore.sql

--- intisp/system/install/sql/999_restore.sql
+++
@@ -1,6 +0,0 @@
-
-INSERT INTO `mail` (`id`, `subject`, `message`) VALUES
-(1,	'Welcome To Webister',	'<b>We are glad that you decided to choose Webister.</b> <p>We hope you enjoy our awesome control panel. You will get messages/emails once you place your email address in the settings.</p><p>\r\nIf you feel that there are some issues or you need fix your Webister, please remember to try updating it first. You can update this in our main control panel.</p>');
-
-INSERT INTO `settings` (`id`, `code`, `value`) VALUES
-(1,	'title',	'My Web Host');
