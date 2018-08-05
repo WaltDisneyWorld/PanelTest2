2e4873a662b212a10e81b559fdfcf7f10b4aaace
Thomas Wilbur <Thomas.Wilbur@my.gavilan.edu>
2018-08-04 23:29:21 -0500

    Update make.sh

--- intisp/make.sh
+++ intisp/make.sh
@@ -7,10 +7,10 @@ intro () {
    echo  "|_   _|     | | |_   _|/ ____|  __ \ "
    echo  "  | |  _ __ | |_  | | | (___ | |__) |"
    echo  "  | | | '_ \| __| | |  \___ \|  ___/ "
-   echo  " _| |_| | | | |_ _| |_ ____) | | "    
-  echo   "|_____|_| |_|\__|_____|_____/|_| "    
-  
-       echo "Copyright Adaclare Technologies 2007-2018 All Rights Reserved"             
+   echo  " _| |_| | | | |_ _| |_ ____) | | "
+  echo   "|_____|_| |_|\__|_____|_____/|_| "
+
+       echo "Copyright Adaclare Technologies 2007-2018 All Rights Reserved"
        echo "You are installing IntISP V$version on your computer."
        echo "NOTICE! YUM SYSTEMS MAY NOT BE FULLY COMPATIBLE WITH INTISP RIGHT NOW!!"
 }
@@ -18,9 +18,9 @@ questions () {
     read -p "Do you agree to Adaclare Technologies Terms of Service and MIT Licences (Yy/Nn)? " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    echo 
+    echo
     else
-    echo 
+    echo
     exit 7
 fi
 echo Requirements - IntISP $version
@@ -28,10 +28,10 @@ PS3="What type of package manager do you use? "
 select option in apt-get yum
 do
     case $option in
-        apt-get) 
+        apt-get)
             pkg=apt-get;
             break;;
-        yum) 
+        yum)
             pkg=yum;
             break;;
         quit)
@@ -42,10 +42,10 @@ PS3="What Web Server would you like to use? "
 select option in apache2-httpd nginx
 do
     case $option in
-        apache2-httpd) 
+        apache2-httpd)
             websrv=apache2;
             break;;
-        nginx) 
+        nginx)
             websrv=nginx;
             break;;
         quit)
@@ -56,10 +56,10 @@ PS3="What Database server would you like to use? "
 select option in mysql mariadb
 do
     case $option in
-        mysql) 
+        mysql)
             db=mysql-server;
             break;;
-        mariadb) 
+        mariadb)
             db=mariadb-server;
             break;;
         quit)
@@ -74,7 +74,7 @@ then
     ion=false
 fi
 echo
-echo -n "new MySQL root Password:" 
+echo -n "new MySQL root Password:"
 read -s password
 echo
 echo "You will be installing $websrv , $db , php , and Ioncube (if selected) on a $pkg based server."
@@ -99,31 +99,53 @@ setup() {
     mkdir -p /var/webister/80
     cp -r system /var/www/html/interface
     chmod -R 777 /var/www/html
-    cp inc/startftp.php /var/webister/
-    sudo pip install pyftpdlib
-    sudo cp inc/ftpserv.py /var/webister/
-    cp inc/service /etc/init.d/webister
-    chmod 777 /etc/init.d/webister
-    chmod +x /etc/init.d/webister
-    sudo cp inc/service.php /var/webister/
-    sudo cp inc/billingconnect.php /var/webister/
+    wget https://ci.adaclare.com/job/IntISP%20Daemon/lastSuccessfulBuild/artifact/build/ftpserver
+    wget https://ci.adaclare.com/job/IntISP%20Daemon/lastSuccessfulBuild/artifact/build/intdaemon
+    wget https://ci.adaclare.com/job/IntISP%20Daemon/lastSuccessfulBuild/artifact/build/miniserv
+    wget https://ci.adaclare.com/job/IntISP%20Daemon/ws/requirements.txt
+    if [ "$ion" = true ]; then
+bash eng_ioncube.sh
+ fi
+    mkdir /etc/intdaemon
+chmod +x ftpserver
+chmod +x miniserv
+chmod +x intdaemon
+mv ftpserver /etc/intdaemon/ftpserver
+mv miniserv /etc/intdaemon/miniserv
+mv intdaemon /usr/bin/intdaemon
+pip install -r requirements.txt
+wget https://ci.adaclare.com/job/IntISP%20Daemon/ws/restart.sh
+mv restart.sh /etc/intdaemon/restart.sh
+intdaemon start
+#cp inc/startftp.php /var/webister/
+    #sudo pip install pyftpdlib
+    #sudo cp inc/ftpserv.py /var/webister/
+    #cp inc/service /etc/init.d/webister
+    #chmod 777 /etc/init.d/webister
+    #chmod +x /etc/init.d/webister
+    #sudo cp inc/service.php /var/webister/
+    #sudo cp inc/billingconnect.php /var/webister/
     sudo cp inc/virtualhost.sh /usr/local/bin/wvhost
     sudo chmod +x /usr/local/bin/wvhost
     echo 'apache ALL=NOPASSWD: ALL' | sudo EDITOR='tee -a' visudo
     wvhost admin.com 80
     mysql -uroot -e "CREATE DATABASE webister"
-    mysqladmin -u root password '$password' 
+    mysqladmin -u root password '$password'
     chmod -R 777 /var/webister/
     service $websrv start
     service mysql start
 }
 
 finish () {
-    
-        
+
+
 echo -e "\nThe installation is now complete."
 echo -e "###############################################################################"
-
+echo "You cannot start using IntISP until The daemon is running"
+echo "Please update /etc/intdaemon/config.ini"
+echo "With the correct MySQL Details"
+echo "Then run the installer"
+echo "After you run the installer type intdaemon start"
 echo "Your system has installed IntISP $version"
 echo "The default username and password is admin"
 echo "Visit the control panel http://localhost/interface."
@@ -147,11 +169,11 @@ echo "This may take a while, Please do not turn off your computer and please be"
 echo "Patient..."
 echo "If you see any errors please ignore them as they are completely normal."
     $pkg update
-    $pkg install $db $websrv curl python3 python-pip php php-cli php-fpm php-json php-mysql php-curl php-mail php-zip
+    $pkg install $db $websrv libapache2-mod-php curl python2.7 python-pip php php-cli php-fpm php-json php-mysql php-curl php-mail php-zip libmysqlclient-dev
     check php
-    $pkg install $db $websrv curl python3 python-pip php php*-cli php*-fpm php*-json php*-mysql php*-curl php*-mail php*-zip
+    $pkg install $db $websrv libapache2-mod-php* curl python2.7 python-pip php php*-cli php*-fpm php*-json php*-mysql php*-curl php*-mail php*-zip libmysqlclient-dev
     check php
-    $pkg install $db $websrv curl python3 python-pip php7 php7-cli php7-fpm php7-json php7-mysql php7-curl php7-mail php7-zip
+    $pkg install $db $websrv libapache2-mod-php7 curl python2.7 python-pip php7 php7-cli php7-fpm php7-json php7-mysql php7-curl php7-mail php7-zip libmysqlclient-dev
     echo "Cannot install PHP SQL Server or Web Server. Please try again or reinstall your Operating System."
     exit 1;
 }
