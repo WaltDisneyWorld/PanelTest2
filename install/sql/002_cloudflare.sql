23da5a8b40a79425ff8457d9f46695e3423064eb
Gustavo Novaro <gnovaro@users.noreply.github.com>
2019-07-02 21:35:53 +0200

    Better fields types and sizes
    TEXT fields make a resevation for 2MB of text

--- install/sql/002_cloudflare.sql
+++ install/sql/002_cloudflare.sql
@@ -1,6 +1,6 @@
 CREATE TABLE cloudflare (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-username TEXT,
-email TEXT,
-password TEXT
+  id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+  username VARCHAR(100),
+  email VARCHAR(320),
+  password VARCHAR(255)
 )
